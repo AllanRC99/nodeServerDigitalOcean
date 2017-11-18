b899d20ffb3cba1134aa775a4cdd7a74a792e002
AllanRC99 <33769683+AllanRC99@users.noreply.github.com>
2017-11-18 05:56:57 +0000

    update server.js, adding index.html to server

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var app = express();
 
 app.get("/", function(req, res)
 {
-	res.send("servidor funcionando");
+	res.sendFile(__dirname + "/index.html");
 })
 
 app.get("/federer", function(req, res)
